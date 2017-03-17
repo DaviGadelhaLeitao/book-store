30272459abf8835950bf89b6871be3d6ba699972
Davi Leitão <davi.leitao@hotmail.com>
2017-03-17 17:09:13 -0300

    Creating a method to return JSON data for products

--- src/main/java/br/com/bookstore/controller/ProductsController.java
+++ src/main/java/br/com/bookstore/controller/ProductsController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -76,6 +77,12 @@ public class ProductsController {
 		Product product = productDAO.find(id);
 		modelAndView.addObject("product", product);
 		return modelAndView;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/{id}")
+	public Product detailJSON(@PathVariable("id") Integer id) {
+		return productDAO.find(id);
 	}
 	
 	
