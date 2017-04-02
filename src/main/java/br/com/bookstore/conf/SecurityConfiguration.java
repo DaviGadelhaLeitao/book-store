ade48eb37837bb34b13687e9d892d2acc0dcd2eb
Davi Leitão <davi.leitao@hotmail.com>
2017-04-01 23:21:09 -0300

    Granting access to the application

--- src/main/java/br/com/bookstore/conf/SecurityConfiguration.java
+++ src/main/java/br/com/bookstore/conf/SecurityConfiguration.java
@@ -21,7 +21,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers("/$2a$10$clgpvXf9HwBPQet6d0IwMeBfotEBOUiCDZi85/u077R3TDzd./Q76").permitAll()
-	    .antMatchers("/products/form").hasRole("ADMIN")
+	    .antMatchers("/products/form").permitAll()
 	    .antMatchers("/cart/**").permitAll()
 	    .antMatchers(HttpMethod.POST, "/products").permitAll()
 	    .antMatchers(HttpMethod.GET, "/products").permitAll()
